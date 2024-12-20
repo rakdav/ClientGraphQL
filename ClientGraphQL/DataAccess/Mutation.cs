957040c73a87b4715fe44bc15c42e97219918235
Andrews <rakdav@inbox.ru>
2024-12-20 11:22:51 +0200

    2

---
+++ ClientGraphQL/DataAccess/Mutation.cs
@@ -0,0 +1,46 @@
+﻿using GraphQL;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+using ModernHttpClient;
+using Newtonsoft.Json;
+
+namespace ClientGraphQL.DataAccess
+{
+    public class Mutation
+    {
+        private static GraphQLHttpClient graphQLHttpClient;
+        static Mutation()
+        {
+            var uri = new Uri("http://localhost:5000/graphql");
+            var graphQLOptions = new GraphQLHttpClientOptions
+            {
+                EndPoint = uri,
+                HttpMessageHandler = new NativeMessageHandler(),
+            };
+            graphQLHttpClient = new GraphQLHttpClient(graphQLOptions,
+                new NewtonsoftJsonSerializer());
+        }
+        public static async Task<T> ExecuteMutationAsync<T>(string graphQLQueryType,
+          string completeQueryString)
+        {
+            try
+            {
+                var request = new GraphQLRequest
+                {
+                    Query = completeQueryString
+                };
+                var response = await graphQLHttpClient.SendMutationAsync<object>(request);
+                var stringResult = response.Data.ToString();
+                stringResult = stringResult!.Replace($"\"{graphQLQueryType}\":", string.Empty);
+                stringResult = stringResult.Remove(0, 1);
+                stringResult = stringResult.Remove(stringResult.Length - 1, 1);
+                var result = JsonConvert.DeserializeObject<T>(stringResult);
+                return result!;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
