389dc9666ecf39b0a04ba07e3ce5a5b09204e933
Andrews <rakdav@inbox.ru>
2024-12-20 11:32:10 +0200

    3

---
+++ ClientGraphQL/DataAccess/Model/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExampleGraphQL.Models
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Author { get; set; }
+        public Guid PostId { get; set; }
+        public Post? Post { get; set; }
+
+        public override string? ToString()
+        {
+            return $"Content:{Content}, Author:{Author}," +
+                $" CreatedAt:{CreatedAt}";
+        }
+    }
+}
---
+++ ClientGraphQL/DataAccess/Model/Post.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExampleGraphQL.Models
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+       
+        public string? Title { get; set; }
+       
+        public string? Content { get; set; }
+      
+        public DateTime? CreateAt { get; set; }
+        public string? Author { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+
+        public override string? ToString()
+        {
+            return $"Content:{Content}, Author:{Author}," +
+                $" Title:{Title}";
+        }
+    }
+}
