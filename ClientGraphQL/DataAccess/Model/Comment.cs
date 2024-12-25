8b8308deee840cd260d398287aba7e1610ae4fa7
rakdav <rakdav@inbox.ru>
2024-12-25 09:31:42 +0200

    Добавьте файлы проекта.

---
+++ ClientGraphQL/DataAccess/Model/Comment.cs
@@ -0,0 +1,24 @@
+﻿
+using ClientGraphQL.DataAccess.Model;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClientGraphQL.DataAcess.Model
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string? Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? Author { get; set; }
+        [Required]
+        public int PostId { get; set; }
+        public Post? Post { get; set; }
+
+        public override string? ToString()
+        {
+            return $"Content:{Content}, Author:{Author}," +
+                $" CreatedAt:{CreatedAt}";
+        }
+    }
+}
---
+++ ClientGraphQL/DataAccess/Model/Post.cs
@@ -0,0 +1,21 @@
+﻿using ClientGraphQL.DataAcess.Model;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClientGraphQL.DataAccess.Model
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public DateTime? CreateAt { get; set; }
+        public string? Author { get; set; }
+        public ICollection<Comment>? Comments { get; set; }
+        public override string? ToString()
+        {
+            return $"Content:{Content}, Author:{Author}," +
+                $" Title:{Title}";
+        }
+    }
+}
